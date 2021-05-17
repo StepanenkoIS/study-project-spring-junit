d0787ab80050b9fcad62774e72419a9731e12491
StepanenkoIS <Citizen(18)>
2021-05-17 17:06:15 +0400

    gitIgnore

---
+++ src/main/java/is/studyprojectspringjunit/StudyProjectSpringJunitApplication.java
@@ -0,0 +1,13 @@
+package is.studyprojectspringjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectSpringJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyProjectSpringJunitApplication.class, args);
+	}
+
+}
