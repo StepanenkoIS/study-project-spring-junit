a742b04403464071df17033dd3e2e9d9c94735bd
istepanenko <Pilot(188)!>
2021-05-18 11:54:32 +0300

    bug fixes

--- src/main/java/is/studyprojectspringjunit/service/ClientDAO.java
+++ src/main/java/is/studyprojectspringjunit/service/ClientDAO.java
@@ -13,22 +13,26 @@ public class ClientDAO {
     }
 
     public Client getByIdClient(ClientRepository clientRepository, Long id) {
-        Client client = new Client();
+        Client client;
         System.out.println("RequestParam " + id);
         try {
             client = clientRepository.getById(id);
+            System.out.println(client);
         } catch (SQLGrammarException exception) {
             System.out.println("table or view does not exist. ID = " + id);
+            client = new Client();
             client.setId(0L);
             client.setLastName("table or view does not exist");
             return client;
         } catch (EntityNotFoundException exception) {
             System.out.println("object not found. ID = " + id);
+            client = new Client();
             client.setId(0L);
             client.setLastName("object not found");
             return client;
         } catch (Exception exception) {
             System.out.println("Exception ID = " + id);
+            client = new Client();
             client.setId(0L);
             client.setLastName("exception");
             return client;
